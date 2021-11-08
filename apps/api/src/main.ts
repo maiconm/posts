03d3df69b5fcd4208b5503360b725559dca79bbb
maiconm <maiconm23@gmail.com>
2021-11-08 01:20:44 +0000

    Instala o client do MongoDB no servidor Express.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,15 @@
 import * as express from 'express';
 import * as cors from 'cors';
 
+import { MongoClient } from 'mongodb';
+
+MongoClient.connect(
+  'mongodb://angular-aula03_devcontainer_db_1:27017',
+).then((client: MongoClient) => {
+  app.locals.db = client.db('artigaria');
+  console.log('Conectado ao MongoDB.');
+});
+
 const app = express();
 
 app.use(cors());
