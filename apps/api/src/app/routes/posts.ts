1159890f90f899706d7946d7cc78cac6583368ee
maiconm <maiconm23@gmail.com>
2021-11-13 17:45:03 +0000

    Adiciona `requireJwtToken` na API de criar posts

--- apps/api/src/app/routes/posts.ts
+++ apps/api/src/app/routes/posts.ts
@@ -5,11 +5,15 @@ import {
   Router,
 } from 'express';
 
+import * as jsonWebToken from 'jsonwebtoken';
+
 import {
   Post,
 } from '@posts/common';
 
 import { getCollection } from '../util/mongodb';
+import { requireJwtToken } from '../middlewares/jwt';
+import { JWT_SECRET_KEY } from '../util/jwt';
 
 export const postsRouter = Router();
 
@@ -21,8 +25,11 @@ postsRouter.get('/', async (req: Request, res: Response, next: NextFunction) => 
   res.json(posts);
 });
 
-postsRouter.post('/', async (req: Request, res: Response, next: NextFunction) => {
+postsRouter.post('/', requireJwtToken, async (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.headers.authorization.split(' ')[1]
+  const { login } = jsonWebToken.verify(authorization, JWT_SECRET_KEY);
   const post: Post = req.body;
+  post.user = login;
   post.date = Date.now();
   const { insertedId } = await getCollection<Post>(
     req.app,
