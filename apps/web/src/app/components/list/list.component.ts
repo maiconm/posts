d2437b66a29c88cc68964a65125d32ab56fb2525
maiconm <maiconm23@gmail.com>
2021-11-13 16:07:53 +0000

    Implementa PostService no ListComponent

--- apps/web/src/app/components/list/list.component.ts
+++ apps/web/src/app/components/list/list.component.ts
@@ -3,6 +3,8 @@ import {
 } from '@angular/core';
 
 import { Post } from '@posts/common';
+import { Observable } from 'rxjs';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'posts-list',
@@ -11,35 +13,12 @@ import { Post } from '@posts/common';
 })
 export class ListComponent {
 
-  public posts = [
-    new Post(
-      '129387123',
-      65465465,
-      'https://i.pinimg.com/originals/10/7a/97/107a97ca5bd4a571edcebec54a66fc32.jpg',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ex recusandae voluptatibus temporibus, quisquam iusto laudantium expedita minima doloribus officiis corporis cum nobis aliquam eligendi beatae qui suscipit voluptas odio.',
-      'Nome do usuario'
-    ),
-    new Post(
-      '129387123',
-      57357375,
-      'https://i.pinimg.com/originals/10/7a/97/107a97ca5bd4a571edcebec54a66fc32.jpg',
-      '',
-      'teste'
-    ),
-    new Post(
-      '129387123',
-      19519159519,
-      '',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ex recusandae voluptatibus temporibus, quisquam iusto laudantium expedita minima doloribus officiis corporis cum nobis aliquam eligendi beatae qui suscipit voluptas odio.',
-      'teste'
-    ),
-    new Post(
-      '129387123',
-      687832159519,
-      'q',
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ex recusandae voluptatibus temporibus, quisquam iusto laudantium expedita minima doloribus officiis corporis cum nobis aliquam eligendi beatae qui suscipit voluptas odio.',
-      'teste'
-    ),
-  ]
+  public posts$: Observable<Post[]>;
+
+  constructor(
+    private postService: PostService
+  ) {
+    this.posts$ = this.postService.posts$.asObservable();
+  }
 
 }
