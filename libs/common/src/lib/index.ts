ad29dc798e04322f2d827b44254324ba7a670159
maiconm <maiconm23@gmail.com>
2021-11-13 17:05:00 +0000

    Introduz coleção de User no MongoDB.

--- libs/common/src/lib/index.ts
+++ libs/common/src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './models/post';
 export * from './interfaces/post-input';
 export * from './interfaces/post-output';
+export * from './interfaces/user';
---
+++ libs/common/src/lib/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+
+  _id: number;
+
+  login: string;
+
+  password: string;
+
+}
